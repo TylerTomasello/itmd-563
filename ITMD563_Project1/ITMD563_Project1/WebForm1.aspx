eba24be3e3efc03244a7163f50de0eef107124ad
TylerTomasello <ttomasel@hawk.iit.edu>
2018-09-18 11:28:31 -0500

    Add projects to github to start version control for future projects

---
+++ ITMD563_Project1/ITMD563_Project1/WebForm1.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ITMD563_Project1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Four Function Calculator</title>
+</head>
+<body>
+    <form id="form1" method="get" runat="server">
+        <div>
+            <h1>Four Function Calculator</h1>
+            <asp:Label ID="Label1" runat="server" Text="Operand 1"></asp:Label>
+            <asp:TextBox ID="Op1" runat="server"></asp:TextBox>
+            <br />
+
+            <asp:Label ID="Label2" runat="server" Text="Operand 2"></asp:Label>
+            <asp:TextBox ID="Op2" runat="server"></asp:TextBox>
+            <hr />
+
+            <p><b>Choose Calculation</b></p>
+            <asp:Button ID="BtnAdd" runat="server" Onclick="BtnAdd_Click1" Text="+" />
+            <asp:Button ID="BtnSub" runat="server" Onclick="BtnSub_Click" Text="-" />
+            <asp:Button ID="BtnMult" runat="server" OnClick="BtnMult_Click" Text="*" />
+            <asp:Button ID="BtnDivide" runat="server" OnClick="BtnDivide_Click" Text="/" />
+
+            <hr />
+            <asp:Label ID="Finish" runat="server" Text="Result is"></asp:Label>
+            <asp:Label ID="Result" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ITMD563_Project1/ITMD563_Project1/WebForm1.aspx.cs
@@ -0,0 +1,69 @@
+﻿// Tyler Tomasello
+// ITMD 563
+// Project 1
+// 9/17/18
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+// Project 1
+namespace ITMD563_Project1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+        
+        //Function called when the Add button is pressed and adds 
+        //the two numbers that were in the text boxes and converts to ints.
+        protected void BtnAdd_Click1(object sender, EventArgs e)
+        {
+            int oper1 = Convert.ToInt32(Op1.Text);
+            int oper2 = Convert.ToInt32(Op2.Text);
+
+            int result = oper1 + oper2;
+            Result.Text = Convert.ToString(result);
+        }
+
+        //Function called when the Subtract button is pressed and subtracts 
+        //the two numbers that were in the text boxes and converts to ints.
+        protected void BtnSub_Click(object sender, EventArgs e)
+        {
+            int oper1 = Convert.ToInt32(Op1.Text);
+            int oper2 = Convert.ToInt32(Op2.Text);
+
+            int result = oper1 - oper2;
+            Result.Text = Convert.ToString(result);
+        }
+
+        //Function called when the Multiply button is pressed and multiplies
+        //the two numbers that were in the text boxes and converts to ints.
+        protected void BtnMult_Click(object sender, EventArgs e)
+        {
+            int oper1 = Convert.ToInt32(Op1.Text);
+            int oper2 = Convert.ToInt32(Op2.Text);
+
+            int result = oper1 * oper2;
+            Result.Text = Convert.ToString(result);
+        }
+
+        //Function called when the Divide button is pressed and divides
+        //the two numbers that were in the text boxes.
+        //Also, it converts them to double to make the value more accurate.
+        protected void BtnDivide_Click(object sender, EventArgs e)
+        {
+            double oper1 = Convert.ToDouble(Op1.Text);
+            double oper2 = Convert.ToDouble(Op2.Text);
+
+            double result = oper1 / oper2;
+            Result.Text = Convert.ToString(result);
+        }
+    }
+}
