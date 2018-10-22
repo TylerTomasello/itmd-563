edc447829ea375773afd353a32e3c2596a1769b1
TylerTomasello <ttomasel@hawk.iit.edu>
2018-10-22 13:22:53 -0500

    file to create the database

---
+++ Proj5-grad-tomasello/SQLQuery1.sql
@@ -0,0 +1,17 @@
+﻿USE [Mortgages]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Mortgages] (
+    [Principle]            INT           IDENTITY (1, 1) NOT NULL,
+    [Interest Rate]        DECIMAL (3, 2) NOT NULL,
+	[Duration Years]       INT NOT NULL,
+	[Monthly Payment]      DECIMAL (15, 2) NOT NULL
+);
+
+
