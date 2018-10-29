09c4820d496fba5abec18d49c927629dacb9e221
TylerTomasello <ttomasel@hawk.iit.edu>
2018-10-29 12:25:22 -0500

    Start new project

---
+++ Proj6-grad-tomasello/Proj6-grad-tomasello.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Proj6_grad_tomasello.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Proj6-grad-tomasello/Proj6-grad-tomasello/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proj6_grad_tomasello.MvcApplication" Language="C#" %>
