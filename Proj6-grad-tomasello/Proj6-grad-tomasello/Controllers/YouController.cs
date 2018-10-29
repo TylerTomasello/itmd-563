3a4eb4c761cea3010ad8082aebae36255198f34a
TylerTomasello <ttomasel@hawk.iit.edu>
2018-10-29 14:30:04 -0500

    Finished working project. Date and time displays but does not update real time.

---
+++ Proj6-grad-tomasello/Proj6-grad-tomasello/Controllers/YouController.cs
@@ -0,0 +1,75 @@
+﻿using Proj6_grad_tomasello.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+// github repo link: https://github.com/TylerTomasello/itmd-563/tree/master/Proj6-grad-tomasello
+namespace Proj6_grad_tomasello.Controllers
+{
+    public class YouController : Controller
+    {
+        
+        // GET: You
+        public ActionResult ViewBagNames()
+        {
+            List<string> list = new List<string>
+            {
+                "Tyler",
+                "Steve",
+                "Tom"
+            };
+            
+
+            // Display Date and time but does not update
+            ViewData["DateTime"] = DateTime.Now.ToString("dddd, MMMM dd yyyy hh:mm:ss tt");
+
+            ViewBag.Message = "Main view of names";
+            ViewBag.Names = list;
+
+            return View();
+        }
+
+        // GET: NameList
+        public ActionResult List()
+        {
+            List<Name> namesList = new List<Name>
+            {
+                new Name()
+                {
+                    FirstName = "Tyler",
+                    LastName = "Tomasello",
+                    CourseName = "Intermediate Web App Dev"
+                },
+
+                new Name()
+                {
+                    FirstName = "Steve",
+                    LastName = "Rogers",
+                    CourseName = "History"
+                },
+
+                new Name()
+                {
+                    FirstName = "Tom",
+                    LastName = "Tanks",
+                    CourseName = "Engineering"
+                }
+            };
+            ViewBag.Message = "Hard Code the list";
+
+            // Display Date and time but does not update
+            ViewData["DateTime"] = DateTime.Now.ToString("dddd, MMMM dd yyyy hh:mm:ss tt");
+
+            return View(namesList);
+        }
+        public ActionResult Create()
+        {
+            // Display Date and time but does not update
+            ViewData["DateTime"] = DateTime.Now.ToString("dddd, MMMM dd yyyy hh:mm:ss tt");
+
+            return View();
+        }
+    }
+}
---
+++ Proj6-grad-tomasello/Proj6-grad-tomasello/Models/Name.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+// github repo link: https://github.com/TylerTomasello/itmd-563/tree/master/Proj6-grad-tomasello
+namespace Proj6_grad_tomasello.Models
+{
+    public class Name
+    {
+        [DisplayName("First Name")]
+        [Required]
+        public virtual string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
+        [Required]
+        public virtual string LastName { get; set; }
+
+        [DisplayName("Name of Course")]
+        [Required]
+        public virtual string CourseName { get; set; }
+    }
+}
