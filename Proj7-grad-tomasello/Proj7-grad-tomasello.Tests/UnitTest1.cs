ae3b0f3d767aeac79a5943d0adffcba320a7a55e
TylerTomasello <ttomasel@hawk.iit.edu>
2018-11-25 16:37:39 -0600

    add controller and tests

--- Proj7-grad-tomasello/Proj7-grad-tomasello.Tests/UnitTest1.cs
+++ Proj7-grad-tomasello/Proj7-grad-tomasello.Tests/UnitTest1.cs
@@ -6,9 +6,46 @@ namespace Proj7_grad_tomasello.Tests
     [TestClass]
     public class UnitTest1
     {
+        //Tested if each year and rate worked with 0
         [TestMethod]
-        public void TestMethod1()
+        public void TestZeroYear15()
         {
+            double mort = CalcMortgage.CalcMonthlyPayment(0, 15, .25);
+            Assert.AreEqual(0.00, mort);
+        }
+
+        [TestMethod]
+        public void TestZeroYear30()
+        {
+            double mort = CalcMortgage.CalcMonthlyPayment(0, 30, .25);
+            Assert.AreEqual(0.00, mort);
+        }
+
+        [TestMethod]
+        public void TestZeroYearOther()
+        {
+            double mort = CalcMortgage.CalcMonthlyPayment(0, 45, .25);
+            Assert.AreEqual(0.00, mort);
+        }
+
+        //Sample test of using rate 1%
+        [TestMethod]
+        public void TestRate1()
+        {
+            double mort = CalcMortgage.CalcMonthlyPayment(1000, 15, 1.00);
+            double f = (double)mort;
+
+            Assert.AreNotEqual(5.98, mort);
+        }
+
+        //Sample test using rate 2%
+        [TestMethod]
+        public void TestRate2()
+        {
+            double mort = CalcMortgage.CalcMonthlyPayment(1000, 15, 2.00);
+            double f = (double)mort;
+
+            Assert.AreNotEqual(6.44, mort);
         }
     }
 }
